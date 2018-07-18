c08e334635ddad61bc544b0638fbde23857d8c39
Vasuki <vasuki.k@gmail.com>
2018-07-18 16:19:32 -0400

    changes to Selenium program

--- seleniumFirst.java
+++ seleniumFirst.java
@@ -1,30 +1,59 @@
-import org.openQA.selenium.WebDriver;
-import org.openQA.selenium.chrome.chromeDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class seleniumFirst
-{
-	publis static void main(String[] args)
-	{
-		WebDriver driver = new ChromeDriver();
-		//launch new browser session and directs to URL that is specified as parameter.
-		driver.get("http://www.google.com");
-		//The WebDriver class has the getTitle() method that is always used to obtain the page title of the currently 
-		//loaded page.
-		
-		String actualtitle = driver.getTitle();
-		String expectedtitle = "Welcome : google";
-		
-		//String Comparision
-		
-		if(expectectedtitle.contentEquals(actualtitle))
-			System.out.println("Test Passed");
-		else
-			System.out.println("Test Failed");
-			//finding GUI elements
-			String tagName;
-			tagName = driver.findElement(By.id("email")).getTagName();
-			System.out.println("Tag Name "+tagName);
-			//closes driver
-			driver.close();
-	}
+public class seleniumFirst {
+/**
+* @param args
+*/
+       public static void main(String[] args) {
+             
+// objects and variables instantiation
+              WebDriver driver = new FirefoxDriver();
+              String appUrl = "https://accounts.google.com";
+             
+// launch the firefox browser and open the application url
+              driver.get(appUrl);
+             
+// maximize the browser window
+              driver.manage().window().maximize();
+             
+// declare and initialize the variable to store the expected title of the webpage.
+              String expectedTitle = " Sign in - Google Accounts ";
+             
+// fetch the title of the web page and save it into a string variable
+              String actualTitle = driver.getTitle();
+             
+// compare the expected title of the page with the actual title of the page and print the result
+              if (expectedTitle.equals(actualTitle))
+              {
+                     System.out.println("Verification Successful - The correct title is displayed on the web page.");
+              }
+             else
+              {
+                     System.out.println("Verification Failed - An incorrect title is displayed on the web page.");
+              }
+
+// enter a valid username in the email textbox
+              WebElement username = driver.findElement(By.id("Email"));
+              username.clear();
+              username.sendKeys("TestSelenium");
+             
+// enter a valid password in the password textbox
+              WebElement password = driver.findElement(By.id("Passwd"));
+              password.clear();
+              password.sendKeys("password123");
+             
+// click on the Sign in button
+              WebElement SignInButton = driver.findElement(By.id("signIn"));
+              SignInButton.click();
+             
+// close the web browser
+              driver.close();
+              System.out.println("Test script executed successfully.");
+             
+// terminate the program
+              System.exit(0);
+       }
 }
